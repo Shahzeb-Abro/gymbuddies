8ce265ce9a3793c4ad48a75b86a93a032d8aeed8
Shahzeb-Abro <khizr772@gmail.com>
2025-06-27 15:39:13 +0500

    Set up tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello World 2</h1>
+      <h1 className="text-3xl text-center text-emerald-500 font-bold underline">
+        Hello World 2
+      </h1>
     </>
   );
 }
